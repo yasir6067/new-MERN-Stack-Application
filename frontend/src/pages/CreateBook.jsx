58bbf5bb9e6fc69fc41d49a9d65d28d090f4382d
yasir6067 <167060208+yasir6067@users.noreply.github.com>
2024-08-28 23:36:10 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -21,7 +21,7 @@ const CreateBooks = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:5656/books", data)
+      .post("https://new-mern-stack-application.onrender.com/books", data)
       .then(() => {
         setLoading(false);
         navigate("/");
